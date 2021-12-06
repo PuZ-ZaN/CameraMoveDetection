017054c9bd5f651d2036ee9d05e9e72f103b1506
PuZ-ZaN <puzzan@protonmail.com>
2021-12-06 19:56:44 +0500

    жестко задан порт и хост

--- CameraMoveDetection/FlaskApp.py
+++ CameraMoveDetection/FlaskApp.py
@@ -2,9 +2,10 @@ from os import environ
 from CameraApp import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    #HOST = environ.get('SERVER_HOST', 'localhost')
+    #try:
+    #    PORT = int(environ.get('SERVER_PORT', '5555'))
+    #except ValueError:
+    #    PORT = 5555
+    #app.run(HOST, PORT)
+    app.run('localhost', 5555)
